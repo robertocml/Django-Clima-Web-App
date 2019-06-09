c335750281e0df450ef2fe23f0aa343362b1e387
Roberto Perez <roberto.perezb@outlook.com>
2019-06-09 15:26:34 -0500

    OpenWeatherMap Api instead of DarkSky

--- El_Clima/Clima/views.py
+++ El_Clima/Clima/views.py
@@ -2,9 +2,21 @@ import requests
 from django.shortcuts import render
 
 def index(request):
-    url = 'https://api.darksky.net/forecast/af08feb858612b09132c8f531f1bd340/25.4333,-101' 
-    city = 'Las Vegas'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metrics&lang=es&APPID=076e9519efb070c5ddf4e3b5a1e3d6ba'
+    city = 'Saltillo,Mx'
 
-    r = requests.get(url.format(city))
-    print(r.text)
-    return render(request, 'Clima/Clima.html')
+    r = requests.get(url.format(city)).json()
+
+    celsius = r['main']['temp'] - 273
+    celsius = round(celsius,1)
+
+    city_weather = {
+        'city': city,
+        'temperature': celsius ,
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon'],
+    }
+
+    context = {'city_weather' : city_weather}
+
+    return render(request, 'Clima/Clima.html',context)
