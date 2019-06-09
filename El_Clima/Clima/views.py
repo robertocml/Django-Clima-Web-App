fe457430d1b104bc18e3cfa68825e5923e7d54b8
Roberto Perez <roberto.perezb@outlook.com>
2019-06-09 02:28:22 -0500

    DarkySky api test

--- El_Clima/Clima/views.py
+++ El_Clima/Clima/views.py
@@ -1,4 +1,10 @@
+import requests
 from django.shortcuts import render
 
 def index(request):
+    url = 'https://api.darksky.net/forecast/af08feb858612b09132c8f531f1bd340/25.4333,-101' 
+    city = 'Las Vegas'
+
+    r = requests.get(url.format(city))
+    print(r.text)
     return render(request, 'Clima/Clima.html')
