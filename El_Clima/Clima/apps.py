d41dcdab3a578a16d5a3a62317ab1def4128c436
Roberto Perez <roberto.perezb@outlook.com>
2019-06-09 00:48:37 -0500

    Initial Commit

---
+++ El_Clima/Clima/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ClimaConfig(AppConfig):
+    name = 'Clima'
--- El_Clima/El_Clima/wsgi.py
+++ El_Clima/El_Clima/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Clima project.
+WSGI config for El_Clima project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Clima.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'El_Clima.settings')
 
 application = get_wsgi_application()
