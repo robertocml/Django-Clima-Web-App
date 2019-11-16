a25074cb15918d83e644fdfca1de2689eb3842ec
Roberto Perez <roberto.perezb@outlook.com>
2019-11-15 22:57:58 -0600

    Version 2

--- El_Clima/Clima/views.py
+++ El_Clima/Clima/views.py
@@ -1,22 +1,53 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metrics&lang=es&APPID=076e9519efb070c5ddf4e3b5a1e3d6ba'
-    city = 'Monterrey,Mx'
+    err = False
+    #city = 'Saltillo,Mx'
 
-    r = requests.get(url.format(city)).json()
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
 
-    celsius = r['main']['temp'] - 273
-    celsius = round(celsius,1)
+    form = CityForm()
 
-    city_weather = {
-        'city': city,
-        'temperature': celsius ,
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
+    cities = City.objects.all()
 
-    context = {'city_weather' : city_weather}
+    weather_data = []
 
-    return render(request, 'Clima/Clima.html',context)
+    try:
+        err = False
+        for city in cities:
+
+            r = requests.get(url.format(city)).json()
+
+            celsius = r['main']['temp'] - 273
+            celsius = round(celsius, 1)
+            temp_max = r['main']['temp_max'] - 273
+            temp_max = round(temp_max, 1)
+            temp_min = r['main']['temp_min'] - 273
+            temp_min = round(temp_min, 1)
+
+            city_weather = {
+                'city': city.name,
+                'temperature': celsius,
+                'tempMax': temp_max,
+                'tempMin': temp_min,
+                'description': r['weather'][0]['description'],
+                'icon': r['weather'][0]['icon'],
+            }
+
+            weather_data.append(city_weather)
+    except KeyError:
+        pass
+    except Exception as e:
+        pass
+
+    print(weather_data)
+
+    context = {'weather_data': weather_data, 'form': form}
+    return render(request, 'Clima/Clima2.html', context)
